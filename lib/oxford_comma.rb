4652ac14358c2a48721d1c403d06e41be7a48b6e
Sarah Williams <srwilliams8678@gmail.com>
2019-04-20 01:35:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,9 @@
 def oxford_comma(array)
- array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"].join
-return array
+  if array.length == 1
+    return ["kiwi"].join
+  elsif array.length == 2
+    return ["kiwi", "durian"].join(" and ")
+  else
+    array[0...-1].join(", ") <<", and #{array[-1]}"
+  end
 end
